be65a48bb1d3c56901cbb2a85e70178661e36001
eurichisabely <141160195+eurichisabely@users.noreply.github.com>
2025-06-16 09:19:04 -0300

    ok

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
-        enunciado: "Qual a importância do 2PAC na cultura de plantações e ervas medicinais indigenas?",
+        enunciado: "Em algumas culturas africanas tradicionais, certas plantas com propriedades psicoativas eram usadas em rituais espirituais e medicinais. Como essa prática se diferencia do uso recreativo de drogas na sociedade moderna e qual o papel das ervas medicinais africanas nesse contexto?",
         alternativas: [
             {
-            texto: "Trazer dignidade e expandir a visibilidade cultural  afro-americana.",
-            afirmacao: "O 2PAC foi fundamental para a cultura afro-americana. "
+            texto: "A principal diferença reside na intenção e no contexto do uso. Nas culturas africanas, o uso de plantas psicoativas era frequentemente ritualístico, guiado por anciãos e visava a cura ou a conexão espiritual, enquanto o uso recreativo de drogas modernas é muitas vezes individual, sem orientação e com foco na alteração da consciência.",
+            afirmacao: " As ervas medicinais africanas, nesse sentido, eram vistas como ferramentas sagradas para o bem-estar da comunidade, e não como substâncias para fins de evasão ou vício."
             },
             {
             texto: "Alternativa2",
